5c74649b1e187199fd002d2b682ac3a2ebb057f4
Priyasi <priyasi.singh@axelerant.com>
2023-04-18 16:13:53 +0530

    for jenkins

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    "reporter": "mochawesome",
+    "reporterOptions": {
+      "charts": true,
+      "overwrite": false,
+      "html": false,
+      "json": true,
+      "reportDir": "cypress/report/mochawesome-report"
+    }
+
   },
 });
